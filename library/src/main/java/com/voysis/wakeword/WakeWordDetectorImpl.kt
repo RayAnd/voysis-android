3eb9ac6765d6f02da7db3b00de92631ea7b1860a
liamduffy <lduffy@voysis.com>
2019-12-18 14:17:12 +0000

    adding array to buffer, reading 800 shorts form buffer at a time to ensure that window stays consistent at 800.

--- library/src/main/java/com/voysis/wakeword/WakeWordDetectorImpl.kt
+++ library/src/main/java/com/voysis/wakeword/WakeWordDetectorImpl.kt
@@ -7,6 +7,7 @@ import com.voysis.events.WakeWordState.IDLE
 import com.voysis.recorder.SourceManager
 import org.apache.commons.collections.buffer.CircularFifoBuffer
 import org.tensorflow.lite.Interpreter
+import java.nio.ShortBuffer
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicReference
@@ -53,13 +54,16 @@ class WakeWordDetectorImpl(private val interpreter: Interpreter,
     }
 
     private fun processWakeWord(source: SourceManager, callback: (WakeWordState) -> Unit) {
-        val shortBuffer = ShortArray(byteWindowSize)
+        val buffer = ShortBuffer.allocate(byteWindowSize * 2)
         val ringBuffer = CircularFifoBuffer(sampleSize)
+        val shortArray = ShortArray(byteWindowSize)
         source.startRecording()
         var count = 0
         while (source.isRecording() && isActive()) {
-            if (source.read(shortBuffer, 0, byteWindowSize) > 0) {
-                shortBuffer.forEach { ringBuffer.add(it.toFloat()) }
+            source.read(shortArray, 0, byteWindowSize)
+            buffer.put(shortArray)
+            if (buffer.position() >= byteWindowSize) {
+                fillRingBuffer(buffer, ringBuffer)
                 if (ringBuffer.size >= sampleSize) {
                     val input = ringBuffer.toArray().map { it as Float }.toFloatArray()
                     count = if (processWakeword(input)) count + 1 else 0
@@ -77,6 +81,13 @@ class WakeWordDetectorImpl(private val interpreter: Interpreter,
         }
         state.set(IDLE)
         callback.invoke(state.get())
+    }
+
+    private fun fillRingBuffer(buffer: ShortBuffer, ringBuffer: CircularFifoBuffer) {
+        val tempArray = ShortArray(byteWindowSize)
+        buffer.get(tempArray, 0, byteWindowSize)
+        tempArray.forEach { ringBuffer.add(it.toFloat()) }
+        buffer.compact()
     }
 
     private fun processWakeword(input: FloatArray): Boolean {
