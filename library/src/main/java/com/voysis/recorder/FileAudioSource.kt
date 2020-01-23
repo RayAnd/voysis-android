4a9ac1841ac387e2c7cf404d7bb48f0831361021
liamduffy <lduffy@voysis.com>
2020-01-23 11:55:58 +0000

    replaced atomic boolean with atomic reference to input stream.

--- library/src/main/java/com/voysis/recorder/FileAudioSource.kt
+++ library/src/main/java/com/voysis/recorder/FileAudioSource.kt
@@ -4,16 +4,14 @@ import com.voysis.recorder.AudioRecorder.Companion.DEFAULT_READ_BUFFER_SIZE
 import java.io.File
 import java.io.IOException
 import java.io.InputStream
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicReference
 
 class FileAudioSource(var wavFile: File? = null) : Source {
 
-    private var inputStream: InputStream? = null
-    private var isActive = AtomicBoolean(false)
+    private var inputStream: AtomicReference<InputStream>? = null
 
     override fun startRecording() {
-        inputStream = wavFile!!.inputStream()
-        isActive.set(true)
+        inputStream = AtomicReference(wavFile!!.inputStream())
     }
 
     override fun generateMimeType(): MimeType? = MimeType(16000, 16, "signed-int", false, 1)
@@ -21,25 +19,21 @@ class FileAudioSource(var wavFile: File? = null) : Source {
     override fun generateBuffer(): ByteArray = ByteArray(DEFAULT_READ_BUFFER_SIZE)
 
     override fun isRecording(): Boolean {
-        return isActive.get()
+        return inputStream?.get() != null
     }
 
     override fun destroy() {
-        synchronized(this) {
-            isActive.set(false)
-            inputStream?.close()
-        }
+        val stream = inputStream?.getAndSet(null)
+        stream?.close()
     }
 
     @Throws(IOException::class)
     override fun read(buffer: ByteArray, i: Int, size: Int): Int {
-        synchronized(this) {
-            val bytesRead = if (isActive.get()) inputStream?.read(buffer, 0, buffer.size)!! else -1
-            if (bytesRead <= -1) {
-                isActive.set(false)
-            }
-            return bytesRead
+        val bytesRead = inputStream?.get()?.read(buffer, 0, buffer.size)!!
+        if (bytesRead <= -1) {
+            destroy()
         }
+        return bytesRead
     }
 
     override fun read(buffer: ShortArray, i: Int, size: Int): Int {
